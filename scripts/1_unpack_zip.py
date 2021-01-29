37f47aaeda510d262b8c2fd92228f6d0495fd69f
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-01-29 16:53:58 +0100

    extracted

--- scripts/1_unpack_zip.py
+++ scripts/1_unpack_zip.py
@@ -1,5 +1,5 @@
 import zipfile
 
 # Unpack Data
-with zipfile.ZipFile("data/raw/short_gutenberg.zip", 'r') as zip_ref:
-    zip_ref.extractall("./data")
+with zipfile.ZipFile("data/1_raw/short_gutenberg.zip", 'r') as zip_ref:
+    zip_ref.extractall("./data/2_extracted")
