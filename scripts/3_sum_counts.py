9dd87124cee20a3f3492879513be09f5bd4ba86b
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-01-29 17:03:34 +0100

    sum_counts

--- scripts/3_sum_counts.py
+++ scripts/3_sum_counts.py
@@ -5,14 +5,14 @@ from pathlib import Path
 import numpy as np
 
 # Sum Word Counts
-data_dir = Path("./data")
+data_dir = Path("./data/3_words_counts")
 total_counts = Counter()
-for f in data_dir.glob('**/*.npy'):
+for f in data_dir.glob('*.npy'):
     data = np.load(f.with_suffix('.npy'), allow_pickle=True)
     dd = dict(data.item())
     total_counts += Counter(dd)
 
-final_dir = Path("./data")
+final_dir = Path("./data/4_final_word_count")
 final_dir.mkdir(exist_ok=True, parents=True)
 final_file = final_dir / "total_word_counts"
 with final_file.with_suffix('.pkl').open('wb') as pickle_file:
