4ca10059cde3d4480ad09c0e3c680247bbc3976d
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-01-29 17:29:47 +0100

    finish dvc workflow

--- scripts/4_gen_plots.py
+++ scripts/4_gen_plots.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+from pathlib import Path
 
 total_counts = np.load("./data/4_final_word_count/total_word_counts.pkl", allow_pickle=True)
 
@@ -8,6 +9,8 @@ total_counts = np.load("./data/4_final_word_count/total_word_counts.pkl", allow_
 plt.figure()
 plt.barh(*zip(*total_counts.most_common(20)))
 plt.title("Most Common Words in Dataset")
+
+Path("results").mkdir(exist_ok=True, parents=True)
 plt.savefig("results/counts.png")
 
 # # Compare Conjunctions
