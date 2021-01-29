173158583a2e77d9d43d753a01fe159e601ac537
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-01-29 17:00:12 +0100

    word_counts

--- scripts/2_word_count.py
+++ scripts/2_word_count.py
@@ -4,12 +4,14 @@ from pathlib import Path
 import numpy as np
 
 # Save Word Count, for each work, into a numpy file
-data_dir = Path(".")
-for f in data_dir.glob('data/**/*.txt'):
+data_dir = Path("data/2_extracted")
+output_dir = Path("data/3_word_counts")
+for f in data_dir.glob('**/*.txt'):
     try:
         text = f.read_text()
     except UnicodeDecodeError:
         continue
-    words = "".join(c for c in text if c not in "\".;:,!$-'1234567890?[](){}><^%=_ã©&").replace('\n', ' ').lower().split()
+    words = "".join(c for c in text if c not in "\".;:,!$-'1234567890?[](){}><^%=_ã©&").replace('\n',
+                                                                                                ' ').lower().split()
     counts = Counter(words)
-    np.save(f.with_suffix('.npy'), counts)
+    np.save(output_dir.joinpath(f.name).with_suffix('.npy'), counts)
