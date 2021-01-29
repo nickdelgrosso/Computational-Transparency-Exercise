ecda451a2d5945207995dc60a2b2d30aa7978aed
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-01-29 16:52:16 +0100

    raw data folder

--- scripts/1_unpack_zip.py
+++ scripts/1_unpack_zip.py
@@ -1,5 +1,5 @@
 import zipfile
 
 # Unpack Data
-with zipfile.ZipFile("data/short_gutenberg.zip", 'r') as zip_ref:
+with zipfile.ZipFile("data/raw/short_gutenberg.zip", 'r') as zip_ref:
     zip_ref.extractall("./data")
